128829b8efdc8b293dc1872a2986fee118ecdab4
coolabhays <abhaysp9955@gmail.com>
2022-08-28 18:45:18 +0530

    feat: create bean for PasswordEncoder

--- spring_security_jpa/src/main/java/com/rlj/service/AppUserDetailsService.java
+++ spring_security_jpa/src/main/java/com/rlj/service/AppUserDetailsService.java
@@ -3,9 +3,12 @@ package com.rlj.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.rlj.dao.AppUserDetails;
@@ -18,6 +21,15 @@ public class AppUserDetailsService implements UserDetailsService
 
 	@Autowired
 	private UserRepository repository;
+
+	// since a password encoder will be used to encode the raw password before
+	// storing, this is the suitable place for this bean (and also avoid
+	// circular dependency)
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder(11);
+	}
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
