fbd79ca7be86d4cbbda75938311e6da3e3f88b12
coolabhays <abhaysp9955@gmail.com>
2022-08-30 22:29:56 +0530

    feat: implementation of interception of incoming request
        demonstration is working
        (still using WebSecurityConfigurerAdapter (deprecated) and few other
        deprecated methods for Jwts)

---
+++ spring_security_jwt/src/main/java/com/rlj/util/JwtRequestFilter.java
@@ -0,0 +1,65 @@
+package com.rlj.util;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.rlj.service.MyUserDetailsService;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter
+{
+
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+			FilterChain filterChain) throws ServletException, IOException
+	{
+        // TODO Auto-generated method stub
+		final String authorizationHeader = request.getHeader("Authorization");
+
+		String username = null;
+		String jwt = null;
+
+		if ((null != authorizationHeader && !authorizationHeader.isEmpty())
+				&& authorizationHeader.startsWith("Bearer ")) {
+			jwt = authorizationHeader.substring(7);
+			username = this.jwtUtil.extractUsername(jwt);
+		}
+
+		if (null != username && null == SecurityContextHolder.getContext().getAuthentication()) {
+			UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+			if (jwtUtil.validateToken(jwt, userDetails)) {
+				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+					// how does these two differ ? (using this overload of
+					// constructor because it sets authenticated to true, I
+					// think)
+					// new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+					new UsernamePasswordAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
+				usernamePasswordAuthenticationToken
+					.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+			}
+		}
+		filterChain.doFilter(request, response);
+    }
+
+
+
+}
