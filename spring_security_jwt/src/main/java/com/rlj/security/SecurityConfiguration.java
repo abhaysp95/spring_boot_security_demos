e88b5996891578d4ea2e8b86c6c965aa9823109a
coolabhays <abhaysp9955@gmail.com>
2022-08-31 15:18:47 +0530

    feat: updated spring security configuration

---
+++ spring_security_jwt/src/main/java/com/rlj/security/SecurityConfiguration.java
@@ -0,0 +1,64 @@
+package com.rlj.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.rlj.service.MyUserDetailsService;
+import com.rlj.util.JwtRequestFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration
+{
+
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
+	@Autowired
+	private MyUserDetailsService userDetailsService;
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+	{
+
+		http.csrf().disable()
+			.authenticationProvider(authenticationProvider())
+			.authorizeRequests().antMatchers("/authenticate").permitAll()
+			.anyRequest().authenticated()
+			.and()
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+		throws Exception
+	{
+		return authenticationConfiguration.getAuthenticationManager();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider()
+	{
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService);
+		authProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+
+		return authProvider;
+	}
+
+}
