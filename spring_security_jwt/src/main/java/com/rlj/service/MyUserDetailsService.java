855a4a00c3ff147cb02b0007039de5392e3a4931
coolabhays <abhaysp9955@gmail.com>
2022-08-31 15:56:09 +0530

    fix: update the demo password

--- spring_security_jwt/src/main/java/com/rlj/service/MyUserDetailsService.java
+++ spring_security_jwt/src/main/java/com/rlj/service/MyUserDetailsService.java
@@ -15,7 +15,8 @@ public class MyUserDetailsService implements UserDetailsService
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // TODO Auto-generated method stub
-        return new User("foo", "bar", Arrays.asList());
+        // return new User("foo", "bar", Arrays.asList());
+        return new User("foo", "$2b$11$KCmo1E.gyOTeD7QJkFv/TO/4lvKvqrbPHy1EW9wJ5ZkLqh09jJDeG", Arrays.asList());
     }
 
 }
