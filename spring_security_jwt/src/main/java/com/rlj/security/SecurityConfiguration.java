60b4f541848809ca6d05f0c62ffb8ba69576e48d
coolabhays <abhaysp9955@gmail.com>
2022-08-31 15:55:20 +0530

    feat: using proper password encoder

--- spring_security_jwt/src/main/java/com/rlj/security/SecurityConfiguration.java
+++ spring_security_jwt/src/main/java/com/rlj/security/SecurityConfiguration.java
@@ -9,7 +9,9 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -56,9 +58,14 @@ public class SecurityConfiguration
 	{
 		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 		authProvider.setUserDetailsService(userDetailsService);
-		authProvider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		authProvider.setPasswordEncoder(passwordEncoder());
 
 		return authProvider;
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder(11);
+	}
 }
